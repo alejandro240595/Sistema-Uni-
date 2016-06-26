3d9701e0284dd1c6911d305e49d74655563a8931
alejandro240595 <ale240595@gmail.com>
2016-06-26 18:30:05 -0500

    java avance 2

--- Clase 1/EurekaApp/src/pe/egcc/eurekaApp/db/AccesoDB.java
+++ Clase 1/EurekaApp/src/pe/egcc/eurekaApp/db/AccesoDB.java
@@ -1,11 +1,23 @@
 
 package pe.egcc.eurekaApp.db;
 
+//Objetivo de la clase de esta clase es que tenga un metodo que permita
+//el acceso a un objeto de tipo connection
 
-public class AccesoDB 
 
+public final class AccesoDB 
 
 {
+    //Constructor privado para que no permita crear objetos de esta clase
+    private AccesoDB()
+    {
+        
+    }
     
+    //Parametros de conexion
+    private static final String CN_DRIVER = "oracle.jdbc.OracleDriver";
+    private static final String CN_URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String CN_USER = "eureka";
+    private static final String CN_PASS = "admin";
     
 }
